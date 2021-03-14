779b4a0886ddd0ad79f13e0cb200dcf2b7dc6e1e
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-03-14 21:17:06 +0700

    Create SimpleCircle.java

---
+++ Pertemuan1/Teori/SimpleCircle.java
@@ -0,0 +1,22 @@
+package Pertemuan1.Teori;
+
+/**Dilla Rahmadika Putri A11.2019.11741*/
+class SimpleCircle {
+    double radius;
+    
+    SimpleCircle(){
+        radius = 1;
+    }
+    SimpleCircle(double newRadius){
+        radius = newRadius;
+    }
+    double getArea(){
+        return radius * radius * Math.PI ;
+    }
+    double getPerimeter(){
+        return 2 * radius * Math.PI ;
+    }
+    void setRadius(double newRadius){
+        radius = newRadius;
+    }
+}
