f1ffce63992f27343876dac35420da59314bd002
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-03-14 21:19:38 +0700

    Create matematikaDemo.java

---
+++ Pertemuan2/Teori/Slide21/matematikaDemo.java
@@ -0,0 +1,21 @@
+package Pertemuan2.Teori.Slide21;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class matematikaDemo {
+    public static void main(String[] args){
+        matematika hitung = new matematika();
+        hitung.pertambahan(20, 20);
+        hitung.Pengurangan(10, 5);
+        hitung.perkalian(10, 20);
+        hitung.pembagian(20, 2);
+    }
+}
