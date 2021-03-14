1b41b2d4c29669d8cc1a533099bfe9e9a2460296
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-03-14 21:19:24 +0700

    Create matematika.java

---
+++ Pertemuan2/Teori/Slide21/matematika.java
@@ -0,0 +1,28 @@
+package Pertemuan2.Teori.Slide21;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class matematika {
+    int hasilnya;
+    
+    void pertambahan(int x,int y){
+        System.out.println(x + " + " + y + " = " + (x+y));
+    }
+    void Pengurangan(int x,int y){
+        System.out.println(x + " - " + y + " = " + (x-y));
+    }
+    void perkalian(int x,int y){
+        System.out.println(x + " x " + y + " = " + (x*y));
+    }
+    void pembagian(int x,int y){
+        System.out.println(x + " / " + y + " = " + (x/y));
+    }
+}
