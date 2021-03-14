994027c50defc74c117e54f1590b4dfa777e8c93
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-03-14 21:19:09 +0700

    Create MobilDemo2.java

---
+++ Pertemuan2/Teori/Slide18/MobilDemo2.java
@@ -0,0 +1,23 @@
+package Pertemuan2.Teori.Slide18;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class MobilDemo2 {
+    public static void main(String[] args){
+        Mobil2 mobil = new Mobil2();
+        mobil.setHidupkan();
+        mobil.GantiGigi(1);
+        mobil.GantiGigi(1);
+        mobil.setMatikan();
+        
+    }
+    
+}
