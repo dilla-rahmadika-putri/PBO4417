0cf44a8af495f93736a42c7d47c55107c72ff348
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-03-23 14:24:44 +0700

    Create GamePlayer.java

---
+++ Pertemuan3/Praktek/GamePlayer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan3.Praktek;
+
+/**
+ *
+ * @author Dilla Rahmadika Putri
+ */
+class GamePlayer {
+    double width;
+    double height;
+    int positionX;
+    int positionY;
+    
+    GamePlayer(){                
+    }
+    GamePlayer(double width,double height){
+   }
+    GamePlayer(double width,double height,int positionX,int positionY){
+    }
+    public void setDimension(double width,double height){
+        this.width = width;
+        this.height =height;
+    }
+    public void setPosition(int positionX,int positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    public double getWidth(){
+        return  this.width;
+    }
+    public double getHeight(){
+        return this.height;
+    }
+    public int getX(){
+        return this.positionX;
+    }
+    public int getY(){
+        return this.positionY;
+    }
+    public void run(){
+        System.out.println("Player is running");
+    }
+    public void run(int incrementX){
+        incrementX = positionX + incrementX;
+        System.out.println("Player still running...current X position= "+incrementX);
+                
+    }            
+}
