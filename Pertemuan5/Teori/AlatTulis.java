ae1a20b5e4b0184929e0208456d5eea855d1f337
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-04-04 18:51:11 +0700

    Create AlatTulis.java

---
+++ Pertemuan5/Teori/AlatTulis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan5.Teori;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AlatTulis {
+    private String Barang; 
+    
+    AlatTulis(){
+        this.Barang = " ";
+    }
+    public String setBarang(String Barang){
+        return this.Barang = Barang;
+    }
+    public String getBarang(){
+        return Barang;
+    }
+}
