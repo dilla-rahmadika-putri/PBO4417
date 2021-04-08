01a94b72160ed0f3997f252acbe14f471c33728f
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-04-08 14:54:39 +0700

    Create Bicycle.java

---
+++ Pertemuan6/Teori/Transportasi/Bicycle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Bicycle {
+    public Bicycle(){
+    }
+    public void lewat(){
+        System.out.println("Sepeda mau lewat");
+    }
+    public void bell(){
+        System.out.println("Kring...Kring...");
+    }
+}
