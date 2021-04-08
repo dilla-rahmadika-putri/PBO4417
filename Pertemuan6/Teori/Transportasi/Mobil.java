90ed1450adb32e07214b824b5b25651540c7bc0a
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-04-08 14:54:58 +0700

    Create Mobil.java

---
+++ Pertemuan6/Teori/Transportasi/Mobil.java
@@ -0,0 +1,27 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mobil {
+    String HidupkanMobil;
+    String MatikanMobil;
+    
+    public Mobil(){
+    }
+    public void jalan(){
+        System.out.println("Maju");
+    }
+    public void setHidupkan(){
+        System.out.println("Mobil Hidup");
+    }
+    public void setMatikan(){
+        System.out.println("Mobil Mati");
+    }
+}
