172a20a2b5c0fbf5b81f350b2d89b09ba763c32e
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-05-11 18:03:20 +0700

    Create MataKuliah.java

---
+++ pertemuan8/MataKuliah.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class MataKuliah extends Mahasiswa{
+    
+    MataKuliah(){  
+    }
+    public void matkul1(){
+        System.out.println("Basis Data");
+    }
+    public void mattam1(){
+        System.out.println("Bahasa Inggris");
+    }
+}
