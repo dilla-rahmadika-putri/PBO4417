99add8b4ec13c8841e54904768f992b1ccb11c92
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-05-11 18:01:48 +0700

    Create Jadwal.java

---
+++ pertemuan8/Jadwal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Jadwal extends Mahasiswa{
+    Jadwal(){
+    }
+     public void jadwal1(){
+         System.out.println("Senin 13.00 - 14.30");
+     }
+     public void jadwal2(){
+         System.out.println("Selasa 09.30 - 11.00");
+     }
+     public void jadwal3(){
+         System.out.println("Rabu 07.30 - 09.00");
+     }
+     public void jadwal4(){
+         System.out.println("Kamis 10.30 - 12.00");
+     }
+     public void jadwal5(){
+         System.out.println("Jum'at 14.00 - 16.00");
+     }
+}
