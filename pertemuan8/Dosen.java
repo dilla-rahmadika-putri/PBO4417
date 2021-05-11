89f1160670949a4dec358f2f554cfa48da151150
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-05-11 18:01:29 +0700

    Create Dosen.java

---
+++ pertemuan8/Dosen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Dosen extends Manusia{
+    String nip;
+    
+    Dosen(){
+    }
+    public String getNip(){
+        return nip;
+    }
+    public void setNip(String nip){
+        this.nip = nip;
+    }
+}
