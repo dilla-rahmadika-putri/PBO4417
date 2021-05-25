38a10184d92f8f25c130f8ece61d7a1197300d48
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-05-25 20:10:12 +0700

    Create Kalkulator.java

---
+++ pertemuan10/praktek/Kalkulator.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan10.praktek;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author ASUS
+ */
+public class Kalkulator extends Frame implements ActionListener {
+    TextField tf1,tf2,tf3;
+    Button b1,b2;
+    
+    Kalkulator(){
+        tf1 = new TextField();
+        tf1.setBounds(50, 50, 150, 20);
+        tf1.setText("Masukan angka");
+        tf2 = new TextField();
+        tf2.setBounds(50, 100, 150, 20);
+        tf2.setText("Masukan angka");
+        tf3 = new TextField();
+        tf3.setBounds(50, 150, 150, 20);
+        tf3.setText("Hasilnya");
+        tf3.setEditable(false);
+        b1 = new Button("+");
+        b1.setBounds(50, 200, 50, 50);
+        b2 = new Button("-");
+        b2.setBounds(120, 200, 50, 50);
+        
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        
+        add(tf1);
+        add(tf2);
+        add(tf3);
+        add(b1);
+        add(b2);
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String s1 = tf1.getText();
+        String s2 = tf2.getText();
+        int a = Integer.parseInt(s1);
+        int b = Integer.parseInt(s2);
+        int c = 0;
+        if (e.getSource()==b1){
+            c = a+b;
+        }
+        else if(e.getSource()==b2){
+            c = a-b;
+        }
+        String result = String.valueOf(c);
+        tf3.setText(result);
+    }
+    public static void main(String[]args){
+        new Kalkulator();
+    }
+}
