f6dd86bec0923a75947681872f401b983189d98b
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-06-18 20:49:56 +0700

    Create ModelUser.java

---
+++ pertemuan13/praktek/LatihanMVC/src/Model/ModelUser.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ModelUser {
+    private int no;
+    private int nik;
+    private String nama;
+    private String tinggal;
+    private String jk;
+    private int usia;
+    private String alasan;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public int getNik() {
+        return nik;
+    }
+
+    public void setNik(int nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTinggal() {
+        return tinggal;
+    }
+
+    public void setTinggal(String tinggal) {
+        this.tinggal = tinggal;
+    }
+
+    public String getJk() {
+        return jk;
+    }
+
+    public void setJk(String jk) {
+        this.jk = jk;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+
+    public void setUsia(int usia) {
+        this.usia = usia;
+    }
+
+    public String getAlasan() {
+        return alasan;
+    }
+
+    public void setAlasan(String alasan) {
+        this.alasan = alasan;
+    }
+}
