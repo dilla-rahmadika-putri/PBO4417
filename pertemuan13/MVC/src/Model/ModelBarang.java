ffa18d894bbf627f742729a24b31cc26ae41a96a
dilla-rahmadika-putri <111201911741@mhs.dinus.ac.id>
2021-06-18 14:09:16 +0700

    Create ModelBarang.java

---
+++ pertemuan13/MVC/src/Model/ModelBarang.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ModelBarang {
+    private int no;
+    private String nama;
+    private Double harga;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+}
